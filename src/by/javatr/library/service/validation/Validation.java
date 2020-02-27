2a76d5acfc31d39e1f6bd25d57a7df28b5c8fae7
RGReeTy <be.a.reety@gmail.com>
2020-02-27 21:38:51 +0300

    delete unusual dependence

--- src/by/javatr/library/service/validation/Validation.java
+++ src/by/javatr/library/service/validation/Validation.java
@@ -1,7 +1,5 @@
 package by.javatr.library.service.validation;
 
-import by.javatr.library.bean.User;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,13 +8,6 @@ public class Validation {
     private final static int PLUS_STEP_FOR_CHAR = 13;
     private final static String CORRECT_SYMBOLS = "[a-zA-Z0-9]{1,20}$";
 
-    public static boolean checkTheUserOnAuth(String login, String password, User user) {
-        if (user == null) {
-            return false;
-        }
-        return user.getUserName().equalsIgnoreCase(login)
-                & user.getUserPassword().equalsIgnoreCase(cryptThePassword(password));
-    }
 
     public static String cryptThePassword(String password) {
         String cryptedWord = "";
@@ -26,6 +17,7 @@ public class Validation {
         }
         return cryptedWord;
     }
+
 
     public static boolean checkAllSymbolsOnLetterOrDigit(String login, String password) {
         Pattern pattern = Pattern.compile(CORRECT_SYMBOLS);
