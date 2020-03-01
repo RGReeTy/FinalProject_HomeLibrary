868c573ae3140bf59ffda30f7acdc6e2677676d3
RGReeTy <be.a.reety@gmail.com>
2020-03-01 22:00:13 +0300

    added some code/refactoring

--- src/by/javatr/library/view/Menu.java
+++ src/by/javatr/library/view/Menu.java
@@ -5,12 +5,10 @@ import by.javatr.library.controller.Controller;
 import static by.javatr.library.view.ScannerDataFromConsole.enterStringFromConsole;
 
 public class Menu {
-    Controller controller = new Controller();
-    String response;
-    String request;
-    final String WELCOME_TEXT = "Welcome to the library!";
-    final String OPTIONS_USER = "You can choose next commands:\n[SHOW]\t[FIND]\t[ADD_BOOK]\t[DELETE_BOOK]\t[QUIT]";
-    final String BUE_TEXT = "Have a nice day!";
+    private Controller controller = new Controller();
+    private final String WELCOME_TEXT = "Welcome to the library!";
+    private final String OPTIONS_USER = "You can choose next commands:\n[SHOW]\t[FIND]\t[ADD_BOOK]\t[DELETE_BOOK]\t[QUIT]";
+    private final String BUE_TEXT = "Have a nice day!";
 
     public void run() {
         String access;
@@ -26,12 +24,12 @@ public class Menu {
     private void workWithLibrary() {
         System.out.println(OPTIONS_USER);
 
-        request = enterStringFromConsole();
+        String request = enterStringFromConsole();
 
         if (request.equalsIgnoreCase("QUIT")) {
             System.out.println(BUE_TEXT);
         } else {
-            response = controller.executeTask(request);
+            String response = controller.executeTask(request);
             System.out.println(response);
             workWithLibrary();
         }
